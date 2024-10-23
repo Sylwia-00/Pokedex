2f80f6a2bedc65d8fee2bf1e4682423e1997b850
Sylwia-00 <kaplon.sylwia@gmail.com>
2024-10-23 10:05:47 +0200

    ajout de l'interface et définition du typage

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,9 @@
-function PokemonCard() {
-	const pokemon = pokemonList[1];
+interface PokemonCard {
+	name : string;
+	imgSrc? : URL;
+}
+
+function PokemonCard ({pokemon}){
 	return (
 		<figure>
 			{pokemon.imgSrc ? (
@@ -12,17 +16,6 @@ function PokemonCard() {
 	);
 }
 
-const pokemonList = [
-	{
-		name: "bulbasaur",
 
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-
-	{
-		name: "mew",
-	},
-];
 
 export default PokemonCard;
